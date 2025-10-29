3198b9ac5012daa39e937c350d428375fa119db3
Owen Wal <ownvndwl@gmail.com>
2025-10-29 15:53:38 +0100

    initial submit

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div>
+      <h1>Welcome to Owen van de Wal's Website</h1>
+      <p>This is the index page.</p>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
